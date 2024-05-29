46d25385a967d153fef9a52c8b70eb0695b78263
rcasalins <92891280+rcasalins@users.noreply.github.com>
2024-05-29 13:04:05 -0500

    Main window

---
+++ main.py
@@ -0,0 +1,29 @@
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="PyGObject Example")
+        
+        # Set the default size of the window
+        self.set_default_size(700, 700)
+        
+        # Create a label and add it to the window
+        label = Gtk.Label(label="Hello, PyGObject!")
+        self.add(label)
+        
+        # Optional: Set border width
+        self.set_border_width(10)
+        
+    def on_destroy(self, widget):
+        Gtk.main_quit()
+
+# Create an instance of the window
+win = MyWindow()
+# Connect the destroy event to the on_destroy method
+win.connect("destroy", win.on_destroy)
+# Show all widgets in the window
+win.show_all()
+# Start the GTK main loop
+Gtk.main()
