10d3406162faa67432fb42770081af77ff41ed14
rcasalins <92891280+rcasalins@users.noreply.github.com>
2024-05-30 00:37:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,22 +13,27 @@ class MyPersonalStatsWindow(Gtk.Window):
         header_bar.props.title = "My Personal Stats"
         self.set_titlebar(header_bar)
 
-        # Create a button in the header bar
+        # Create the "+" button in the header bar
         add_button = Gtk.Button()
         add_button.set_image(Gtk.Image.new_from_icon_name("list-add-symbolic", Gtk.IconSize.BUTTON))
+        add_button.connect("clicked", self.on_add_button_clicked)
         header_bar.pack_start(add_button)
 
         # Create a box to hold widgets
         self.box = Gtk.Box(spacing=6)
         self.add(self.box)
 
-        # Add a button to the window
-        self.button = Gtk.Button(label="Click Me")
-        self.button.connect("clicked", self.on_button_clicked)
-        self.box.pack_start(self.button, True, True, 0)
+    def on_add_button_clicked(self, widget):
+        # Create a square button in the middle of the window
+        square_button = Gtk.Button(label="Square")
+        self.box.pack_start(square_button, True, True, 0)
+        square_button.connect("clicked", self.on_square_button_clicked)
 
-    def on_button_clicked(self, widget):
-        print("Button clicked!")
+        # Show all widgets
+        self.show_all()
+
+    def on_square_button_clicked(self, widget):
+        print("Square button clicked!")
 
 win = MyPersonalStatsWindow()
 win.connect("destroy", Gtk.main_quit)
